7bb6b7f6427e65dbb8493cc389ec7f3e32f25773
Jerry Johnson <j.johnson@criteo.com>
2024-09-11 14:02:19 -0400

    fix broken rule, set Angular/Typescript as optionalPeerDeps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:eslint-plugin/recommended', 'plugin:node/recommended'],
+  extends: ['eslint:recommended', 'plugin:eslint-plugin/recommended', 'plugin:n/recommended'],
   env: {
     node: true,
   },
